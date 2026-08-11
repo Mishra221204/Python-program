d950f232d3b3438efd59ac84547f4ad9aec5f6bd
Shubham Mishra <mishrashubham76395@gmail.com>
2026-08-11 04:05:41 -0400

    Add files via upload

---
+++ mymodule.py
@@ -0,0 +1,18 @@
+# creat code to use in another- py file
+
+def say_hello(name,age):
+    return  print(f'name is {name}  in {age} year old!')
+
+def say_bye(name):
+    return  print(f'By {name} takecare!')
+
+
+#  used varible
+
+person={'name': 'shubham','age': 20}
+
+name='shubham'
+age=24
+addresh='saraideeh'
+
+person3=(f'My name is {name} in {age} year and Addresh {addresh}')
