978e220941a0af4465d2a2a045f0638519da6949
Shubham Mishra <mishrashubham76395@gmail.com>
2026-08-11 02:51:26 -0400

    Add files via upload

---
+++ input.py
@@ -0,0 +1,46 @@
+# input function in python
+# user enter  
+# input function always read input value as a string
+
+# a=input()
+# print(a+a)
+# print(type(a+a))
+# print(a+a)
+# print(int(a)+int(a))
+
+#  str convert bool
+# print(bool(a))
+# print(type(a))
+
+
+# name=input("enter  your name")
+# print(f" welcome to  {name}  in  python  tutorial ")
+
+# detail=input("Enter a detail")
+# print(f"Welcom back shubham is big boss in {detail}")
+
+
+# age=input("enter your age:")
+# print(f" Ohh your age {age}")
+
+# print(f" so next year your {int(age)+1}")
+
+# print(f"2027 me your age in {int(age)+3}")
+
+
+#  Q1.- 3 variable print
+
+name=input("Enter your name:")
+age=input("Enter your age:")
+Addres=input("Enter your Addres")
+
+print(f"My name is {name}  form {Addres}  and your age {age}")
+
+
+#  Next year your age change
+age=input("Enter your age:")
+print(f"My name is {name}  from  {Addres} and your age  {int(age)+1}")
+
+#  change name
+name=input("Enter a name :")
+print(f" new name is {name}  from  {Addres} and your age  {int(age)}")
