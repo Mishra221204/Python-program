127484e5e50ce6fe317a64ad73315a501e40143e
Shubham Mishra <mishrashubham76395@gmail.com>
2026-08-11 02:47:34 -0400

    Add files via upload

---
+++ first.py
@@ -0,0 +1,50 @@
+from posixpath import sep
+
+
+print("hello world")
+
+#  this is variable
+# number value ko add kre stke hai (quote ke under)
+# a="12"
+# b="12"
+# print(a+b)  # add nhi hoga
+
+# c="shubham"
+# d=123
+# print(c,d)
+
+print("4567")
+
+
+#  This is variable
+instructor="Shubham Mishra"
+print("hello ", instructor)
+
+value="Shubham"
+print("Welcome back ",value)
+
+
+
+# Sepret used display space
+
+# instructor="Shubham Mishra"
+# print("hello ", instructor,sep='-')
+
+
+# value="Shubham"
+# print("Welcome back ",value,sep='__')
+
+
+# add , multiply ,divide  (calculator proble) 
+
+# add
+a=12
+b=33
+c=143
+print(a+b+c)
+
+# diivide
+print(10/2)
+
+# multiply
+print(20*2)
